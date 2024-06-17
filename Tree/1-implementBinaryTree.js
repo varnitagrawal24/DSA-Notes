9de63dd387d3f214af0cb4d6dfb285dea0c95f2d
Varnit Agrawal <varnit.agrawal@napses.com>
2024-06-17 12:31:07 +0530

    tree: implemeantaion, travresal and height of tree

---
+++ Tree/1-implementBinaryTree.js
@@ -0,0 +1,87 @@
+class TreeNode{
+    constructor(val){
+        this.data = val;
+        this.left = this.right = null;
+    }
+}
+
+export class BinaryTree{
+    constructor(data){
+        this.root = new TreeNode(data);
+    }
+
+    // methods
+    insert(data){
+        if(this.root === null){
+            this.root = new TreeNode(data);
+            return;
+        }
+        const tempQueue = [];
+        tempQueue.push(this.root);
+
+        while(tempQueue.length){
+            const tempNode = tempQueue.shift();
+            if(tempNode.left === null){
+                tempNode.left = new TreeNode(data);
+                break;
+            } else {
+                tempQueue.push(tempNode.left);
+            }
+            if(tempNode.right === null){
+                tempNode.right = new TreeNode(data);
+                break;
+            } else {
+                tempQueue.push(tempNode.right);
+            }
+        }
+    }
+
+    inorder(){
+        let result = [];
+        function getInorder(root){
+            if(root !== null){
+                getInorder(root.left);
+                result.push(root.data);
+                getInorder(root.right);
+            }
+        }
+        getInorder(this.root);
+        return result;
+    }
+
+    preorder(){
+        let result = [];
+        function getPreorder(root){
+            if(root !== null){
+                result.push(root.data);
+                getPreorder(root.left);
+                getPreorder(root.right);
+            }
+        }
+        getPreorder(this.root);
+        return result;
+    }
+
+    postorder(){
+        let result = [];
+        function getPostorder(root){
+            if(root !== null){
+                getPostorder(root.left);
+                getPostorder(root.right);
+                result.push(root.data);
+            }
+        }
+        getPostorder(this.root);
+        return result;
+    }
+
+    height(){
+        function getHeight(root){
+            if(root === null) return 0;
+
+            return Math.max(getHeight(root.left), getHeight(root.right))+1;
+        }
+        const result = getHeight(this.root);
+        return result;
+    }
+}
