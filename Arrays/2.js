59dd0961cab0df5efce0e3253e155ce706553362
Varnit Agrawal <varnit.agrawal@napses.com>
2025-01-12 23:12:22 +0530

    add sol to 2nd array q

--- Arrays/2.js
+++ Arrays/2.js
@@ -1,1 +1,13 @@
 // Find the maximum and minimum element in an array
+
+
+function findMinMaxInArray(arr){
+    const l = arr.length
+    let minIndex = 0;
+    let maxIndex = 0;
+    for(let i = 1; i<l; i++){
+        if(arr[minIndex] > arr[i]) minIndex = i;
+        if(arr[maxIndex] < arr[i]) maxIndex = i;
+    }
+    return {minIndex, maxIndex};
+}
